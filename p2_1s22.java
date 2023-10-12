7457339d9ceba305408979f1a4d820cedc157990
oscar vicente gonzalez cruz <144292940+oscarvicen@users.noreply.github.com>
2023-10-11 18:41:26 -0600

    Delete p2_1s22.java

--- p2_1s22.java
+++
@@ -1,9 +0,0 @@
-class p2_1s22{
-	public static void main(String[] args) {
-		System.out.println("Oscar Vicente Gonzalez Cruz");
-		System.out.println("17 años");
-		System.out.println("oscar.gonzalez260509@gmail.com");
-		System.out.println("me gustan los videojuegos");
-		System.out.println("negro");
-	}
-}
