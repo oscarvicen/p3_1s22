144f8c00fb151ef875f8e2646a2a99c2cc16aca9
oscar vicente gonzalez cruz <144292940+oscarvicen@users.noreply.github.com>
2023-10-18 19:23:09 -0600

    Add files via upload

---
+++ p2/p2p6_1s22.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class p2p6_1s21{
+	public static void main(String[] args) {
+		int[] numero = new int[10];
+		int[] calif = new int[10];
+		Scanner teclado = new Scanner(System.in);
+		int promedio;
+		numeros[0]=1;
+		numeros[1]=2;
+		numeros[2]=3;
+		numeros[3]=4;
+		numeros[4]=5;
+		numeros[5]=6;
+		numeros[6]=7;
+		numeros[7]=8;
+		numeros[8]=9;
+		numeros[9]=10;
+
+		System.out.println("el valor en la posision 5: " +numeros[2]);
+
+		int[] numero1=new int[]{0,1,2,3,4,5,6,7,8,9};
+		System.out.println("el tamaño del arrefki es:" + numero1.length);
+		System.out.println("el valor en la posicion 5 es " +numeros1[5]);
+		Scanner teclado = new Scanner(System.in);
+		int[] calif = mew int[10];
+		int a;
+		for (a=0;a=calif.length;a++){
+			System.out.println("ingrese el valor para la posicion #"+a);
+			calif[a]=teclado.nextLine();
+		}
+		int promedio=0;
+		for(a=0;a=calif.length;a++){
+			promedio+=calif[a];
+		}
+		System.out.println("el promedio es: "+(promedio/10));
+		String[] cadenas = new String[10];
+		String[] cadena1 = new String[]{"uno","dos","tres","cuatro","cinco"}
+
+		double[] flotantes = new double[10];
+		double[] flotantes1= new double[]{1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9}
+	}
+}
---
+++ p2/p2p7_1s22.java
@@ -0,0 +1,8 @@
+public class p2p7_1s22{
+	public static void main(String[] args) {
+		System.out.println("el numero de parametros ingresadosdesde linea de cuando"+args.length);
+		for(int a=0;a<args.length;a++){
+			System.out.println("valor #"+a+ ";"+args[a]);
+		}
+	}
+}
